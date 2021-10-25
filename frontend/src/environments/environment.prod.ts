67d3890c1d018b6f1def0d9f3e252dd433a8c743
zarifsoftware <zarifprogrammer@gmail.com>
2021-10-25 20:59:44 +0600

    Some Bug Fixed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   googleOAuthClientId:
     '222780846548-1c2bhoucero1i2dtn2bvkvlvk5d0i43m.apps.googleusercontent.com',
   facebookOAuthClientId: '1804805713032429',
-  apiURL: 'https://83.172.129.23:8000',
+  apiURL: 'https://cardsanddice.nl:8000',
   currency: '€',
 };
